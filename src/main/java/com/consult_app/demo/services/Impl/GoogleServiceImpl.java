18352148fb1c02b2fa53a39e19faa71d48f51958
Admin <phuckhoa33>
2024-03-07 11:04:31 +0700

    Complete google authentication and google calendar allow, config google meet hangout without excute

--- src/main/java/com/consult_app/demo/services/Impl/GoogleServiceImpl.java
+++ src/main/java/com/consult_app/demo/services/Impl/GoogleServiceImpl.java
@@ -4,4 +4,16 @@ import com.consult_app.demo.services.GoogleService;
 
 public class GoogleServiceImpl implements GoogleService {
 
+    @Override
+    public String oauth2Callback(String code) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'oauth2Callback'");
+    }
+
+    @Override
+    public String authorize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'authorize'");
+    }
+
 }
